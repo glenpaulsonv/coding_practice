ff1c7770d2841949ea9931607cc7199bb8fdca7d
glenpaulsonv <glenpaulsonv@gmail.com>
2021-08-21 17:36:24 +0530

    print_all_substrings.java

---
+++ print_all_substrings.java
@@ -0,0 +1,11 @@
+public static void allSubStrings(String str)
+{
+
+    for(int i=0;i<str.length();i++)
+    {
+        for(int j=i+1;j<=str.length();j++)
+        {
+            System.out.println(str.substring(i,j));
+        }
+    }
+}
