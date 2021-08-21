9a106c56d494fe1d37ffe9ceb82c470fc9412cce
glenpaulsonv <glenpaulsonv@gmail.com>
2021-08-21 08:24:49 +0530

    gas_station.java

---
+++ gas_station.java
@@ -0,0 +1,20 @@
+public class Solution {
+	public int canCompleteCircuit(final List<Integer> gas, final List<Integer> cost) {
+	    int currentFuel = 0;
+	    int remaining = 0;
+	    int total = 0;
+	    int start = 0;
+	    for(int i = 0; i < gas.size(); i++){
+	        remaining = gas.get(i) - cost.get(i);
+	        if(currentFuel >= 0)
+	            currentFuel += remaining;
+	        else{
+	            currentFuel = remaining;
+	            start = i;
+	        }
+	        total += remaining;
+	    }
+	    return total >= 0 ? start : -1;
+	}
+}
+//took the help of interviewBit solution
