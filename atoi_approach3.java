3790e0cfd9cedf0d20f9be81c59aee1d43fc29c3
glenpaulsonv <glenpaulsonv@gmail.com>
2021-09-02 11:57:09 +0530

    atoi_approach4.java

--- atoi_approach3.java
+++ atoi_approach3.java
@@ -28,3 +28,34 @@ class atoi
         return sign*res;
     }
 }
+
+//atoi approach 4 - considering all 4 corner cases - leading white spaces, sign, overflow and invalid input
+
+class atoi{
+    static int solveAtoI(char[] str)
+    {
+        int sign=1; base=0;
+        int i=0;
+        while(str[i]==' ')
+        {
+            i++;
+        }
+        if(str[i]=='-'||str[i]=='+')
+        {
+            sign=1 - 2 * (str[i++]=='-'?1:0);
+        }
+
+        while(i<str.length && str[i]>='0' && str[i]<='9')
+        {
+            if(base>Integer.MAX_VALUE/10 || (base==Integer.MAX_VALUE/10 && str[i]-'0' > 7))
+            {
+                if(sign == 1)
+                    return Integer.MAX_VALUE;
+                else
+                    return Integer.MIN_VALUE;
+            }
+            base = 10 * base + str[i++] - '0';
+        }
+        return base*sign;
+    }
+}
